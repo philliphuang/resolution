cc155d37f8308bf59c6d574308d225c6b00ef339
Phillip Huang <philliphuang@college.harvard.edu>
2016-06-21 09:34:34 -0400

    Fix examples.txt and clean up plugin.py
    Fixing bug in examples.txt, cleared some default code in plugin.py

--- resolution.bundle/plugin.py
+++ resolution.bundle/plugin.py
@@ -1,12 +1,9 @@
 def results(fields, original_query):
-  message = fields['~message']
-  return {
-    "title": "Say '{0}'".format(message),
-    "run_args": [message],
-	"html": "<h1 style='font-family: sans-serif; padding: 2em'>{0}</h1>".format(message)
-  }
+	return {
+		"title": fields,
+		"run_args": [],
+		"html": "<h1 style='font-family: sans-serif; padding: 2em'>{0}</h1>".format(fields)
+	}
+	
 
-def run(message):
-  import os, pipes
-  os.system('say "{0}"'.format(pipes.quote(message.encode('utf8'))))
-
+def run():
