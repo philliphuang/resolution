e617893d24378d50bc01609120f2cf2ed3ec0615
Phillip Huang <philliphuang@college.harvard.edu>
2016-06-26 23:43:50 -0400

    scpt script implementation
    run scpt apple script files rather than manual lines of apple script
    via command line

--- plugin.py
+++ plugin.py
@@ -6,33 +6,33 @@ def results(parsed, original_query):
   	# target cases
 	if target in ("increase", "up"):
   		title = "Adjust resolution one step higher"
-  		cmd = "osascript -e 'display dialog \"TODO: AppleScript for increasing resolution one step\"'"
+  		script = "up"
 
 	elif target in ("decrease", "down"):
 		title = "Adjust resolution one step lower"
-  		cmd = "osascript -e 'display dialog \"TODO: AppleScript for decreasing resolution one step\"'"
+  		script = "down"
 
 	elif target in ("maximum", "max"):
 		title = "Adjust resolution to maximum"
-  		cmd = "osascript -e 'display dialog \"TODO: AppleScript for setting resolution to maximum\"'"
+  		script = "max"
 
 	elif target in ("minimum", "min"):
 		title = "Adjust resolution to minimum"
-  		cmd = "osascript -e 'display dialog \"TODO: AppleScript for setting resolution to minimum\"'"
+  		script = "min"
 
   	elif target == "default":
   		title = "Adjust resolution to default"
-  		cmd = "osascript -e 'display dialog \"TODO: AppleScript for setting resolution to default\"'"
+  		script = "default"
 
   	else: 
   		title = "Invalid target resolution"
-  		cmd = "osascript -e 'display dialog \"Invalid target resolution\"'"
+  		script = "invalid"
 
 	return {
 		"title": title,
-		"run_args": [cmd],
+		"run_args": [script],
 		"html": "<h1 style='font-family: sans-serif; padding: 2em'>{0}</h1>".format(title)
 	}
 
-def run(cmd):
-	os.system(cmd)
+def run(script):
+	os.system("osascript {}.scpt".format(script))
